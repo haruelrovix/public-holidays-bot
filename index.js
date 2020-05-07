06921a4741d1a01e107c3c50d4693e72a370d2bd
haruelrovix <havit@adakerja.com>
2020-05-07 14:03:43 +0700

    feat: Send message to Slack channel

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const { google } = require('googleapis')
 const { startOfWeek, endOfWeek } = require('date-fns/fp')
+const { IncomingWebhook } = require('@slack/webhook')
+
+const webhook = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL)
+const sendMessage = async text => await webhook.send({ text })
 
 const getHolidays = async country => {
   try {
@@ -15,16 +19,26 @@ const getHolidays = async country => {
     }
 
     const { data } = await calendar.events.list(params)
-    console.info(`\n${data.summary}`)
+
+    let text = `*${data.summary}*\n`
     data.items.forEach(item => {
-      console.info(`${item.summary} (${item.start.date})`)
-    });
+      text += `${item.summary} (${item.start.date})\n`
+    })
+
+    await sendMessage(text)
   } catch (e) {
     console.error(e)
   }
 }
 
-const countries = ['indonesian', 'singapore', 'indian', 'ir', 'turkish']
-for (const country of countries) {
-  getHolidays(country)
+const showMeHolidays = async () => {
+  const countries = ['indonesian', 'singapore', 'indian', 'ir', 'turkish']
+  for (const country of countries) {
+    await getHolidays(country)
+  }
+
+  sendMessage('_Source: <https://developers.google.com/calendar|Calendar API>_')
 }
+
+sendMessage('Public holidays this week guys <!here|here>! :beach_with_umbrella:')
+showMeHolidays()
